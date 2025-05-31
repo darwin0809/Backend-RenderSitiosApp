067a6314d0783d9c5eab08ef3304962c6405493c
Darwin Andres Criollo Leon <darwin.criollo@uao.edu.co>
2025-05-31 04:04:14 -0500

    fechaVis4

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { registrar, login, eliminarUsuario } = require('../controller/authController'); // ajusta el nombre si es necesario
+const { registrar, login, eliminarUsuario, obtenerUsuarios} = require('../controller/authController'); // ajusta el nombre si es necesario
 
 router.post('/register', registrar);
 router.post('/login', login);
 router.delete('usuarios/:id', eliminarUsuario);
-router.get('/usuarios', verificarToken, obtenerUsuarios); 
+router.get('/usuarios', obtenerUsuarios); 
 
 module.exports = router;
