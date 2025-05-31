5f608cbb0baf6d0ec6b2f6bdb035f02b29e0161c
Darwin Andres Criollo Leon <darwin.criollo@uao.edu.co>
2025-05-30 23:52:43 -0500

    fix: configuración CORS para frontend en Ionic

--- app.js
+++ app.js
@@ -16,7 +16,12 @@ const consultasRoutes = require('./routes/consultaRoutes');
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/api/paises', paisRoutes);
