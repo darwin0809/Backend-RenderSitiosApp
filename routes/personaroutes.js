87d67a2a1b54a17d673e35cdb3499b3592ec1599
Darwin Andres Criollo Leon <darwin.criollo@uao.edu.co>
2025-05-31 01:44:10 -0500

    Solucion Error Iden2

--- routes/personaroutes.js
+++ routes/personaroutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const personaController = require('../controller/personaController');
+const personaController = require('../controller/personacontroller');
 
 router.get('/', personaController.obtenerPersonas);
 router.get('/:id', personaController.obtenerPersonaPorId);
